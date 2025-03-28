770bdcb29fb61e5c5bbeb6116004236a55d188e9
dmitriymc <70806712+dmitriymc@users.noreply.github.com>
2025-03-28 17:15:54 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'blue-primary': '#3b82f6',
+        'purple-primary': '#a5b9fb',
+        'gray-primary': '#b4b4b4',
+        'gray-secondary': '#e5e7eb'
+      },
+      backgroundSize: {
+        '50%': '500%'
+      }
+    },
+  },
+  plugins: [],
+}
+
+// Chat
+// codejr
+// Shop Items
+// Currency
+// Homework
